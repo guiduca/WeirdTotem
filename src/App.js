d4492e412178133b34e6997078c7c377a5717dfa
Val <valentin@boureaud.com>
2018-04-03 18:49:36 +0800

    Create new branch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount(){
+    
+  }
+
   render() {
     return (
       <div className="App">
